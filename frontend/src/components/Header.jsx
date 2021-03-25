bbc6686c517adf0451d5e58365a371b8b803274d
HozefaMJ <hozefa24imp@gmail.com>
2021-03-26 02:01:50 +0530

    FE Header Responsive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -44,13 +44,13 @@ export default function Header() {
         </div>
         <div className="header-nav-menu d-none d-lg-block">
           <ul className="d-flex nav nav-neutral-first justify-content-center">
-            {/*
+            {/*Comment Begins*/}
             <li>
               <a
                 className="font-size-lg text-second font-weight-bold rounded-sm"
                 href="#/"
                 onClick={(e) => e.preventDefault()}>
-                Pages
+                Packages
                 <span className="opacity-5 dropdown-arrow">
                   <FontAwesomeIcon icon={['fas', 'angle-down']} />
                 </span>
@@ -65,7 +65,7 @@ export default function Header() {
                         href="#/"
                         onClick={(e) => e.preventDefault()}
                         className="px-4 d-flex align-items-center">
-                        <span>Courses</span>
+                        <span>Courses Heelo</span>
                         <FontAwesomeIcon
                           icon={['fas', 'angle-right']}
                           className="opacity-6 ml-auto"
@@ -136,188 +136,21 @@ export default function Header() {
                 </div>
               </div>
             </li>
-
-            <li>
-              <a
-                href="#/"
-                onClick={(e) => e.preventDefault()}
-                className="font-size-lg text-second font-weight-bold rounded-sm">
-                Landings
-                <span className="opacity-5 dropdown-arrow">
-                  <FontAwesomeIcon icon={['fas', 'angle-down']} />
-                </span>
-              </a>
-              <div className="submenu-dropdown submenu-dropdown--xxl">
-                <Row className="no-gutters">
-                  <Col lg="6" className="z-over">
-                    <div className="shadow-xxl bg-white rounded">
-                      <div className="px-4 text-uppercase pt-4 pb-2 text-primary font-weight-bold font-size-sm">
-                        Applications
-                      </div>
-                      <Nav
-                        pills
-                        className="nav-neutral-primary nav-lg flex-column px-3 pb-3">
-                        <NavItem className="py-1">
-                          <NavLinkStrap
-                            href="#/"
-                            onClick={(e) => e.preventDefault()}
-                            className="px-3">
-                            <div className="d-flex">
-                              <Briefcase className="text-black-50" />
-                              <div className="pl-3 text-black">
-                                <div className="font-weight-bold">General</div>
-                                <div className="text-black-50 font-size-sm">
-                                  Multi-purpose user interface for dashboards
-                                </div>
-                              </div>
-                            </div>
-                          </NavLinkStrap>
-                        </NavItem>
-                        <NavItem className="py-1">
-                          <NavLinkStrap
-                            href="#/"
-                            onClick={(e) => e.preventDefault()}
-                            className="px-3">
-                            <div className="d-flex">
-                              <Users className="text-black-50" />
-                              <div className="pl-3 text-black">
-                                <div className="font-weight-bold">
-                                  Messenger
-                                </div>
-                                <div className="text-black-50 font-size-sm">
-                                  Niche application UI for building chat windows
-                                </div>
-                              </div>
-                            </div>
-                          </NavLinkStrap>
-                        </NavItem>
-                        <NavItem className="py-1">
-                          <NavLinkStrap
-                            href="#/"
-                            onClick={(e) => e.preventDefault()}
-                            className="px-3">
-                            <div className="d-flex">
-                              <Grid className="text-black-50" />
-                              <div className="pl-3 text-black">
-                                <div className="font-weight-bold">Commerce</div>
-                                <div className="text-black-50 font-size-sm">
-                                  Build a commerce related app with this
-                                  template
-                                </div>
-                              </div>
-                            </div>
-                          </NavLinkStrap>
-                        </NavItem>
-                      </Nav>
-                    </div>
-                  </Col>
-                  <Col lg="6" className="d-flex align-items-center">
-                    <div className="shadow-xxl w-100 bg-dark rounded-right p-3">
-                      <Nav
-                        pills
-                        className="nav-transparent-alt nav-lg flex-column p-0">
-                        <NavItem className="py-1">
-                          <NavLinkStrap
-                            href="#/"
-                            onClick={(e) => e.preventDefault()}
-                            className="text-white-50 px-3">
-                            <div className="d-flex">
-                              <Bell className="text-white-50" />
-                              <div className="pl-3 text-white">
-                                <div className="font-weight-bold">Crypto</div>
-                                <div className="text-white-50 font-size-sm">
-                                  User interface inspired by a cryptocurrency
-                                  exchange
-                                </div>
-                              </div>
-                            </div>
-                          </NavLinkStrap>
-                        </NavItem>
-                        <NavItem className="py-1">
-                          <NavLinkStrap
-                            href="#/"
-                            onClick={(e) => e.preventDefault()}
-                            className="text-white-50 px-3">
-                            <div className="d-flex">
-                              <LifeBuoy className="text-white-50" />
-                              <div className="pl-3 text-white">
-                                <div className="font-weight-bold">
-                                  Learning
-                                  <span className="ml-3 badge badge-danger">
-                                    Soon
-                                  </span>
-                                </div>
-                                <div className="text-white-50 font-size-sm">
-                                  Courses platform template to start development
-                                  faster
-                                </div>
-                              </div>
-                            </div>
-                          </NavLinkStrap>
-                        </NavItem>
-                        <NavItem className="py-1">
-                          <NavLinkStrap
-                            href="#/"
-                            onClick={(e) => e.preventDefault()}
-                            className="text-white-50 px-3">
-                            <div className="d-flex">
-                              <Settings className="text-white-50" />
-                              <div className="pl-3 text-white">
-                                <div className="font-weight-bold">
-                                  Banking
-                                  <span className="ml-3 badge badge-danger">
-                                    Soon
-                                  </span>
-                                </div>
-                                <div className="text-white-50 font-size-sm">
-                                  Financial dashboard template inspired by
-                                  banking apps
-                                </div>
-                              </div>
-                            </div>
-                          </NavLinkStrap>
-                        </NavItem>
-                      </Nav>
-                    </div>
-                  </Col>
-                </Row>
-              </div>
-            </li>
-                        
-            <li>
-              <a
-                className="font-size-lg text-second font-weight-bold rounded-sm"
-                href="#/"
-                onClick={(e) => e.preventDefault()}>
-                Blocks
-              </a>
-            </li>
-
-
-
-
-            className="px-4 d-flex align-items-center">
-                        <span>Workplace</span>
-                        <FontAwesomeIcon
-                          icon={['fas', 'angle-right']}
-                          className="opacity-6 ml-auto"
-                        />
-            */}
             <li className="font-size-lg text-second font-weight-bold rounded-sm">
               <SearchBarNav/>
-            </li>
+            </li>            
           </ul>
         </div>
         <div className="header-nav-actions flex-grow-0 flex-lg-grow-1">
           <span className="d-none d-lg-block">
             <Button outline className="btn-pill m-2" color="second">
-                <span className="btn-wrapper--label">Hozefa</span>
+                <span className="btn-wrapper--label">Hozefa Jaorawala</span>
                 <span className="btn-wrapper--icon">
                     <FontAwesomeIcon icon={['fas', 'angle-down']} />
                 </span>
             </Button>
           </span>
-         {/*
+         {/**/}
           <span className="d-block d-lg-none">
             <button
               onClick={toggle}
@@ -329,9 +162,9 @@ export default function Header() {
               </span>
             </button>
           </span>
-         */}
+         
         </div>
-        {/*
+        
         <div className="d-flex d-lg-none">
           <Collapse
             isOpen={collapse}
@@ -352,11 +185,7 @@ export default function Header() {
                     href="#/"
                     onClick={(e) => e.preventDefault()}
                     className="px-4 d-flex align-items-center">
-                    <span>Courses</span>
-                    <FontAwesomeIcon
-                      icon={['fas', 'angle-right']}
-                      className="opacity-6 ml-auto"
-                    />
+                    <SearchBarNav/>
                   </NavLinkStrap>
                 </NavItem>
                 <NavItem>
@@ -364,57 +193,9 @@ export default function Header() {
                     href="#/"
                     onClick={(e) => e.preventDefault()}
                     className="px-4 d-flex align-items-center">
-                    <span>Listings</span>
+                    <span>Hozefa Jaorawala</span>
                     <FontAwesomeIcon
-                      icon={['fas', 'angle-right']}
-                      className="opacity-6 ml-auto"
-                    />
-                  </NavLinkStrap>
-                </NavItem>
-                <NavItem>
-                  <NavLinkStrap
-                    href="#/"
-                    onClick={(e) => e.preventDefault()}
-                    className="px-4 d-flex align-items-center">
-                    <span>Workplace</span>
-                    <FontAwesomeIcon
-                      icon={['fas', 'angle-right']}
-                      className="opacity-6 ml-auto"
-                    />
-                  </NavLinkStrap>
-                </NavItem>
-                <NavItem>
-                  <NavLinkStrap
-                    href="#/"
-                    onClick={(e) => e.preventDefault()}
-                    className="px-4 d-flex align-items-center">
-                    <span>Software</span>
-                    <FontAwesomeIcon
-                      icon={['fas', 'angle-right']}
-                      className="opacity-6 ml-auto"
-                    />
-                  </NavLinkStrap>
-                </NavItem>
-                <NavItem>
-                  <NavLinkStrap
-                    href="#/"
-                    onClick={(e) => e.preventDefault()}
-                    className="px-4 d-flex align-items-center">
-                    <span>Exchanges</span>
-                    <FontAwesomeIcon
-                      icon={['fas', 'angle-right']}
-                      className="opacity-6 ml-auto"
-                    />
-                  </NavLinkStrap>
-                </NavItem>
-                <NavItem>
-                  <NavLinkStrap
-                    href="#/"
-                    onClick={(e) => e.preventDefault()}
-                    className="px-4 d-flex align-items-center">
-                    <span>Services</span>
-                    <FontAwesomeIcon
-                      icon={['fas', 'angle-right']}
+                      icon={['fas', 'angle-down']}
                       className="opacity-6 ml-auto"
                     />
                   </NavLinkStrap>
@@ -423,7 +204,7 @@ export default function Header() {
             </div>
           </Collapse>
         </div>
-        */}
+        {/**/}
       </div>
       <div
         className={clsx('collapse-page-trigger', { 'is-active': collapse })}
