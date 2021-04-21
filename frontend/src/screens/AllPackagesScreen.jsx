cd8b33e2f8d4bd96f664a11142b0b103c0689b83
HozefaMJ <hozefa24imp@gmail.com>
2021-04-21 07:54:41 +0530

    IN allPackages in All PackageScreen

--- frontend/src/screens/AllPackagesScreen.jsx
+++ frontend/src/screens/AllPackagesScreen.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-
-import { Row, Col, Container, Button } from 'reactstrap';
+import React, {useEffect} from 'react';
+import {useDispatch,useSelector} from "react-redux"
+import { Row, Col, Container } from 'reactstrap';
 
 import Header1 from "../components/Header1";
 import Footer1 from "../components/Footer1";
@@ -8,40 +8,46 @@ import Footer1 from "../components/Footer1";
 import PackageCard from 'components/Cards/PackageCard';
 import AllPackagesPagination from 'components/Pagination/AllPackagesPagination';
 
+import BasicLoader from "../components/LoadingIndicators/BasicLoader";
+import ErrorAlert from "../components/Alerts/ErrorAlert";
+
+import {listPackages} from "../actions/packageActions"
+
 export default function AllPackageScreen() {
+
+  const dispatch = useDispatch()
+
+  const packageList = useSelector((state) => state.packageList)
+
+  const {loading,error,packages} = packageList;
+
+  useEffect(() => {
+    dispatch(listPackages())
+  }, [dispatch])
+
   return (
     <>
       <Header1/>
         <Container>
-          <h2 className="mt-5">Latest Packages:</h2>
-          <Row>
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <PackageCard destination="Mystical Ladakh"/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <PackageCard destination="Mystical Ladakh"/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <PackageCard destination="Mystical Ladakh"/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <PackageCard destination="Mystical Ladakh"/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <PackageCard destination="Mystical Ladakh"/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <PackageCard destination="Mystical Ladakh"/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <PackageCard destination="Mystical Ladakh"/>
-            </Col>
-          </Row>
-          <Row>
+          
+          
+            <h2 className="mt-5">Latest Packages:</h2>
+            {loading ? <BasicLoader loading={loading}/> : error ? <ErrorAlert error={error}/> : (
+               <Row>
+               {packages.map(packagei => (
+                 <Col key={packagei._id} sm={12} md={10} lg={6} xl={3}>
+                   <PackageCard packagei={packagei}/>
+                 </Col>
+             ))}
+             </Row>
+            )}
+           
+            <Row>
               <Col lg={12}>
-                  <AllPackagesPagination/>
+                <AllPackagesPagination/>
               </Col>
-          </Row>
+            </Row>
+         
         </Container>
       <Footer1/>
     </>
